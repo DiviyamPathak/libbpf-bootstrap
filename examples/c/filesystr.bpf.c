c6cf1c7c1d344db09e00cb44cebd1fb1bd337c46
DiviyamPathak <patdiviyam@gmail.com>
2025-11-07 01:55:54 +0530

    add openat syscall tracer

---
+++ examples/c/filesystr.bpf.c
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause
+/* Copyright (c) 2020 Facebook */
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
+
+SEC("tracepoint/syscalls/sys_enter_openat")
+int handle_sys_enter_openat(struct trace_event_raw_sys_enter *ctx)
+{
+    const char *filename = (const char *)ctx->args[1];
+    char fname[256];
+    bpf_probe_read_user_str(fname, sizeof(fname), filename);
+    bpf_printk("openat: %s\n", fname);
+    return 0;
+}
